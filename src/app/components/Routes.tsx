f907909059c96dd7ebdab2f0c04b4ba6cfea267f
Hector4like6gorillaz <78667228+hector4like6gorillaz@users.noreply.github.com>
2022-02-09 17:48:35 -0600

    fotos y maquetado base

--- src/app/components/Routes.tsx
+++ src/app/components/Routes.tsx
@@ -1,29 +1,14 @@
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Home from "../../Pages/Home/Home";
-import Page404 from "../../Pages/Page404/Page404";
-import App from "../../Pages/CounterRedux/App";
 
 const Routes = () => {
-  const senti: boolean = true;
   return (
     <>
       <Switch>
         {/* Home */}
         <Route exact path="/">
-          {senti ? <Home /> : <Redirect to="/404" />}
+          <Home />
         </Route>
-        {/* Counter App */}
-        <Route exact path="/app">
-          {senti ? <App /> : <Redirect to="/404" />}
-        </Route>
-
-        {/*404 not found*/}
-        <Route exact path="/404">
-          {<h1>No se encuentra la pagina</h1>}
-        </Route>
-        <Redirect to="/404" />
-
-        <Route component={Page404} />
       </Switch>
     </>
   );
